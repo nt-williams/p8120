11a7d8f5fe99bfe61a73139548fd8998c57d6f56
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-12-16 15:46:21 -0500

    risk diff function working for simple 2 by 2 tables

--- r/riskdiff.R
+++ r/riskdiff.R
@@ -1,15 +1,23 @@
 
 riskdiff <- function(x, y = NULL, weight = NULL, conf.level = 0.95, 
-                     rev = c("neither", "rows", "columns", "both")) {
+                     rev = c("neither", "rows", "columns", "both"), 
+                     dnn = NULL) {
   
-  if (!is.null(y))
-    dnn <- c(deparse(substitute(x)), deparse(substitute(y)))
-  else if (is.null(y)) 
-    dnn <- names(dimnames(x))
-  
+  # error checks
   if (is.matrix(x) && !is.null(y)) {
     stop("If x is a matrix, y should be NULL")
   }
+  
+  # extracting dimension names
+  if (is.null(dnn)) 
+    if (!is.null(y))
+      dnn <- c(deparse(substitute(x)), deparse(substitute(y)))
+    else if (is.null(y)) 
+      dnn <- names(dimnames(x))
+  else (!is.null(dnn))
+    dnn <- dnn
+    
+  # building table
   if (is.matrix(x)) {
     x <- epitable(x, rev = rev)
   }
@@ -23,14 +31,35 @@ riskdiff <- function(x, y = NULL, weight = NULL, conf.level = 0.95,
   
   names(dimnames(x)) <- dnn
   
-  return(x)
+  # rd and conf interval
+  p1 <- x[1,1] / sum(x[, 1])
+  p2 <- x[1, 2] / sum(x[, 2])
+  rd <- round(dge - ndge, 4)
+  z <- qnorm(0.5 * (1 + conf.level))
+  se <- sqrt((p1*(1 - p1)/sum(x[, 1])) + (p2*(1 - p2)/sum(x[, 2])))
+  if (rd < 0) 
+    ci <- rd + c(-1, 1) * z * se
+  else if (rd > 0)
+    ci <- rd + c(1, -1) * z * se
+  
+  cat("Risk difference:", rd, "\n")
+  cat(paste(conf.level * 100, "%", sep = ""), "confidence interval:", round(ci, 4), "\n")
+  cat("\n")
+  cat("Contingency table: \n")
+  x
+
 }
 
-x <- xtabs(~ azt + aids, data = azt)
+# examples
+x <- xtabs(~ aids + azt, data = azt)
+riskdiff(x = x, rev = "b")
 
-riskdiff(x = x)
+riskdiff(x = x, y = azt$azt)
 
-riskdiff(x = azt$aids, y = azt$azt)
+riskdiff(x = azt$aids, y = azt$azt, conf.level = 0.99, rev = "b")
+
+riskdiff(x = azt$aids, y = azt$azt, conf.level = 0.99, rev = "b", dnn = c("hi", "hello"))
 
 riskdiff(x = esoph$alcgp, y = esoph$agegp, weight = esoph$ncontrols)
+
 
