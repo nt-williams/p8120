09f769db50310109fbff7cfb063772450ba89c89
Nicholas Williams <ntwilliams.personal@gmail.com>
2018-12-17 12:51:14 -0500

    riskdiff now accepts data argument

--- r/riskdiff.R
+++ r/riskdiff.R
@@ -1,5 +1,5 @@
 
-riskdiff <- function(x, y = NULL, weight = NULL, conf.level = 0.95, 
+riskdiff <- function(df, x, y = NULL, weight = NULL, conf.level = 0.95, 
                      rev = c("neither", "rows", "columns", "both"), 
                      dnn = NULL) {
   
@@ -11,30 +11,43 @@ riskdiff <- function(x, y = NULL, weight = NULL, conf.level = 0.95,
   # extracting dimension names
   if (is.null(dnn)) 
     if (!is.null(y))
-      dnn <- c(deparse(substitute(x)), deparse(substitute(y)))
+      if (missing(df)) 
+        dnn <- c(deparse(substitute(x)), deparse(substitute(y)))
+      else dnn <- c(noquote(x), noquote(y))
     else if (is.null(y)) 
       dnn <- names(dimnames(x))
-  else (!is.null(dnn))
-    dnn <- dnn
-    
-  # building table
-  if (is.matrix(x)) {
-    x <- epitable(x, rev = rev)
-  }
-  if (!is.null(y) && is.null(weight)) {
-    x <- table(x, y) %>% 
-      epitable(rev = rev)
-  } else if (!is.null(y) && !is.null(weight)) {
-    x <- xtabs(weight ~ x + y) %>% 
-      epitable(rev = rev)
-  }
+  else dnn <- dnn
+  
+  if (missing(df)) {
+    if (is.matrix(x)) {
+      x <- epitable(x, rev = rev)
+    }
+    if (!is.null(y) && is.null(weight)) {
+      # x <- xtabs(~ x + y) %>% 
+      #   epitable(rev = rev)
+      x <- table(x, y) %>% 
+        epitable(rev = rev)
+    } else if (!is.null(y) && !is.null(weight)) {
+      x <- xtabs(weight ~ x + y) %>% 
+        epitable(rev = rev)
+    }
+  } else
+    if (!is.null(y) && is.null(weight)) {
+      # x <- xtabs(~ df[[x]] + df[[y]]) %>%
+      #   epitable(rev)
+      x <- table(df[[x]], df[[y]]) %>%
+        epitable(rev = rev)
+    } else if (!is.null(y) && !is.null(weight)) {
+      x <- xtabs(df[[weight]] ~ df[[x]] + df[[y]]) %>%
+        epitable(rev = rev)
+    }
   
   names(dimnames(x)) <- dnn
   
   # rd and conf interval
   p1 <- x[1,1] / sum(x[, 1])
   p2 <- x[1, 2] / sum(x[, 2])
-  rd <- round(dge - ndge, 4)
+  rd <- round(p1 - p2, 4)
   z <- qnorm(0.5 * (1 + conf.level))
   se <- sqrt((p1*(1 - p1)/sum(x[, 1])) + (p2*(1 - p2)/sum(x[, 2])))
   if (rd < 0) 
@@ -55,7 +68,10 @@ x <- xtabs(~ aids + azt, data = azt)
 riskdiff(x = x, rev = "b")
 riskdiff(x = x, y = azt$azt)
 riskdiff(x = azt$aids, y = azt$azt, conf.level = 0.99, rev = "b")
+riskdiff(azt, "aids", "azt")
+azt %>% 
+  riskdiff("aids", "azt")
 riskdiff(x = azt$aids, y = azt$azt, conf.level = 0.99, rev = "b", dnn = c("AZT", "AIDS"))
 riskdiff(x = esoph$alcgp, y = esoph$agegp, weight = esoph$ncontrols)
 
-
+catfun::riskdiff(azt, "aids", "azt")
